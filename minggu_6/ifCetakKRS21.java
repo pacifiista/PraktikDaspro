df63723cc255ad09adaac32fc87305bc7974b844
pacifiista <naurasfh2020@gmail.com>
2025-10-07 14:35:48 +0700

    Tugas 1

--- minggu_6/ifCetakKRS21.java
+++ minggu_6/ifCetakKRS21.java
@@ -11,11 +11,19 @@ public class ifCetakKRS21 {
         System.out.println("Apakah UKT sudah lunas? (True/False): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverivikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else{
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu ");
-        }
+        // Proses versi If-Else
+            // if (uktLunas) {
+            //System.out.println("Pembayaran UKT terverivikasi");
+            //System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        //} else{
+           // System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu ");
+        //}
+
+        // Versi Ternary operator
+        String pesan = uktLunas
+             ? "Pembayaran UKT terverivikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+             : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println("\n[Versi Ternary Operator]");
+        System.out.println(pesan);
     }
 }
