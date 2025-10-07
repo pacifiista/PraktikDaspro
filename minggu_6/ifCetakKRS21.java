81b328b43cdcff8cd41e77384625c82a8ac83952
pacifiista <naurasfh2020@gmail.com>
2025-10-07 07:02:06 +0700

    Modifikasi percobaan 1

--- minggu_6/ifCetakKRS21.java
+++ minggu_6/ifCetakKRS21.java
@@ -14,6 +14,8 @@ public class ifCetakKRS21 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverivikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else{
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu ");
         }
     }
 }
