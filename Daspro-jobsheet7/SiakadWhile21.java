8ebab4ef21fdb86b3487cbb1c9272bd360a09f4e
pacifiista <naurasfh2020@gmail.com>
2025-10-21 13:55:13 +0700

    SiakadWhile21

---
+++ Daspro-jobsheet7/SiakadWhile21.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class SiakadWhile21 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    //deklarasi
+    int nilai, jml, i=0;
+
+    // input
+    System.out.print("Masukkan jumlah mahasiswa: ");
+    jml = sc.nextInt();
+    while (i < jml) {
+        System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ":");
+        nilai = sc.nextInt();
+
+        if (nilai < 0 || nilai> 100) {
+            System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
+            continue;
+        }
+        if (nilai >80 && nilai <= 100) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah A");
+            System.out.println("Bagus, pertahankan nilainya!");
+        } else if (nilai >73 && nilai <= 80) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah B+");
+        }else if (nilai >65 && nilai <= 73) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah B");
+        }else if (nilai >60 && nilai <= 65) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah C+");
+        }else if (nilai >50 && nilai <= 60) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah C");
+        }else if (nilai >39 && nilai <= 50) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah D");
+        }else {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + "Adalah E");
+        }
+        i++;
+    }
+}
+}
