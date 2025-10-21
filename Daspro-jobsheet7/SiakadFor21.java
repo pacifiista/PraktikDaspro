a5cce66024424d0ccd403ad5c098afb7863f06af
pacifiista <naurasfh2020@gmail.com>
2025-10-21 13:26:05 +0700

    modifikasi siakadFor21

--- Daspro-jobsheet7/SiakadFor21.java
+++ Daspro-jobsheet7/SiakadFor21.java
@@ -6,7 +6,7 @@ public class SiakadFor21 {
 
     //Deklarasi
     double nilai, tertinggi=0, terendah=100;
-
+    int lulus=0, tidakLulus=0, batasKelulusan=60;
 
     for (int i = 1; i < 10; i++) {
         System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -17,8 +17,15 @@ public class SiakadFor21 {
         if (nilai<terendah) {
             terendah = nilai;
         }
+        if (nilai >= batasKelulusan) {
+            lulus++;
+        } else {
+            tidakLulus++;
+        }
     }
         System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " +lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " +tidakLulus);
     }
 }
