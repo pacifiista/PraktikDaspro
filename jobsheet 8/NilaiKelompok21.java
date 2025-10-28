d25c8daf1e2b32d00e1b5724cdc804b26da088aa
pacifiista <naurasfh2020@gmail.com>
2025-10-28 12:04:46 +0700

    modifikasi percobaan 4

--- jobsheet 8/NilaiKelompok21.java
+++ jobsheet 8/NilaiKelompok21.java
@@ -2,23 +2,29 @@ import java.util.Scanner;
 public class NilaiKelompok21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float totalNilai=0,rataNilai;
-        int nilai;
+        float totalNilai=0,rataNilai, rataTertinggi=0;
+        int nilai, kelompokTertinggi= 0;
 
         int i=1;
         while (i <= 6) {
             System.out.println("Kelompok " + i);
             totalNilai=0;
         
-        for (int j = 1; j <= 5; j++) {
-            System.out.print("Nilai dari kelompok Penilai " + j + ": ");
-            nilai = sc.nextInt();
-            totalNilai += nilai; 
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai; 
             
         }
         rataNilai = totalNilai   / 5;
-        i++;
         System.out.println("Rata-rata nilai kelompok: " +  rataNilai);
+
+        if (rataNilai > rataTertinggi) {
+            rataTertinggi = rataNilai;
+            kelompokTertinggi = i;
+        }
+        i++;
     }
+    System.out.println("\nKelompok dengan rata-rata TErtinggi adalah " + kelompokTertinggi+ "dengan rata-rata sebesar " +rataTertinggi);
 }
 }
