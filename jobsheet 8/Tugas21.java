019efcd8a91d96755397566355a59eebfa0eb4b6
pacifiista <naurasfh2020@gmail.com>
2025-10-28 13:30:39 +0700

    tugas 1

---
+++ jobsheet 8/Tugas21.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tugas21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int jumlah= 0;
+            System.out.print("Jumlah kuadrat 1 s.d " + i + ": ");
+            for (int j = 1; j <= i; j++) {
+                jumlah += j*j;
+                System.out.print(j + "^2");
+                if (j < i) {
+                    System.out.print("+");
+                }
+            }
+            System.out.println(" = " +jumlah);
+        }
+    }
+}
