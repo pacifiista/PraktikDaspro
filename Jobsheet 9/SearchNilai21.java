6bab2b126bf892a1350c53ebb6c155b232245281
pacifiista <naurasfh2020@gmail.com>
2025-11-04 13:47:54 +0700

    modifikasi percobaan 4

--- Jobsheet 9/SearchNilai21.java
+++ Jobsheet 9/SearchNilai21.java
@@ -1,17 +1,36 @@
+import java.util.Scanner;
 public class SearchNilai21 {
     public static void main(String[] args) {
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 0;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
+        int jmlNilai=0;
+
+        // input jumlah nilai
+        System.out.print("Masukkan banyaknya nilai yaang akan diinput: ");
+            jmlNilai = sc.nextInt();
+            int [] arrNilai = new int [jmlNilai];
+        
+        // input nilai mahasiswa
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key ==  arrNilai[i]) {
+            System.out.print("nilai ke-" + (i+1) + ":");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai (key) yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1; // inisialisasi dengan -1 untuk menandakan nilai belum ditemukan
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
                 hasil = i;
-                break;
+                break; 
             }
         }
         System.out.println();
-        System.out.println("Nilai " + key + " keetemu di indeks ke-" +hasil);
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ditemukan, merupakan nilai mahasiswa ke-" +jmlNilai);
+        } else{
+            System.out.println("Nilai " + key + " tidak ditemukan");
+        }
         System.out.println();
     }
 }
