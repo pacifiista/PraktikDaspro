121a3dce7d65feb67ec6321467418cc8f684a013
pacifiista <naurasfh2020@gmail.com>
2025-11-04 11:39:47 +0700

    percobaan 1

---
+++ Jobsheet 9/ArrayBilangan21.java
@@ -0,0 +1,15 @@
+public class ArrayBilangan21 {
+
+    public static void main(String[] args) {
+        int [] bilangan = new int [4];
+
+        bilangan [0] = 5;
+        bilangan [1] = 13;
+        bilangan [2] = -7;
+        bilangan [3] = 17;
+
+        for (int i = 0; i <= 4 ; i++) {
+            System.out.println(bilangan[i]);
+        }
+    }
+}
