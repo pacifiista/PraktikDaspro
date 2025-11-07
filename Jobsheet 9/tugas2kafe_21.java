3d66a2f605f8da8adc54a82b19326f14d7686244
pacifiista <naurasfh2020@gmail.com>
2025-11-07 16:34:24 +0700

    tugas 2 jobsheet 9

--- Jobsheet 9/tugas2kafe_21.java
+++ Jobsheet 9/tugas2kafe_21.java
@@ -13,43 +13,29 @@ public class tugas2kafe_21 {
         // Deklarasi array
         String[] menu = new String[jmlPesanan];
         int[] harga = new int[jmlPesanan];
+        int total=0;
 
         // Input menu dan harga
         for (int i = 0; i < jmlPesanan; i++) {
             System.out.print("Menu " + (i + 1) + ": ");
             menu[i] = sc.nextLine();
             System.out.print("Harga " + (i + 1) + ": ");
-            while (!sc.hasNextInt()) {
-                System.out.println("Masukkan angka untuk harga!");
-                sc.next(); // buang input yang salah
-            }
             harga[i] = sc.nextInt();
             sc.nextLine(); // membersihkan newline
+
+            total += harga[i]; // menghitung total harga
         }
 
         // Menampilkan daftar menu dan total harga
         System.out.println("\nDaftar Menu Anda:");
-        int total = 0;
+        
         for (int i = 0; i < jmlPesanan; i++) {
             System.out.println((i + 1) + ". " + menu[i] + " - Rp. " + harga[i]);
-            total += harga[i];
+            
         }
         System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^");
         System.out.println("Total Harga: Rp" + total);
 
-        // Pencarian menu
-        System.out.print("\nMasukkan Menu pilihan anda: ");
-        String key = sc.nextLine();
-        boolean found = false;
-        for (String item : menu) {
-            if (item.equalsIgnoreCase(key)) {
-                System.out.println("Menu \"" + key + "\" tersedia.");
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("Menu \"" + key + "\" tidak tersedia.");
-        }
+        
     }
 }
