08d60dfca6fbdc82de54868eb2675e661400386d
pacifiista <naurasfh2020@gmail.com>
2025-11-14 12:01:57 +0700

    modifikasi percobaan 4 SIAKAD

--- Jobsheet 11/SIAKAD21.java
+++ Jobsheet 11/SIAKAD21.java
@@ -4,34 +4,74 @@ public class SIAKAD21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int[4][3];
+        // Input jumlah siswa dan jumlah mata kuliah
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMahasiswa = sc.nextInt();
+        System.out.print("Masukkan Jumlah Mata Kuliah: ");
+        int jmlMatkul = sc.nextInt();
 
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahsiswa ke-" + (i+1));
-            double totalPerSiswa = 0;
+        int [][] nilai = new int[jmlMahasiswa][jmlMatkul];
 
-            for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+        //Input nilai per Mahasiswa
+        for (int i = 0; i < jmlMahasiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i+1));
+            double totalNilai = 0;
+        
+
+            for (int j = 0; j < jmlMatkul; j++) {
+                System.out.println("Nilai Mata Kuliah " + (j+1) + ": ");
                 nilai[i][j] = sc.nextInt();
-                totalPerSiswa += nilai[i][j];
+                totalNilai += nilai [i][j];
             }
+            double rataRatasiswa=0;
+            rataRatasiswa = totalNilai / jmlMatkul;
+            System.out.printf("Rata-rata nilai mahasiswa ke-%d: %.2f%n", (i+1), rataRatasiswa);
+        }   
 
-            System.out.println("nilai rata-rata: " + totalPerSiswa/3);
-        }
+        // Rata-rata per Matkul
+        System.out.println("====================");
+        System.out.println("Rata-rata Nilai setiap Mata kuliah: ");
 
-        System.out.println("\n===================================");
-        System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
-
-        for (int j = 0; j < 3; j++) {
-            double totalPerMatkul = 0;
-            
-            for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < jmlMatkul; j++) {
+            double totalPerMatkul=0;
+            for (int i = 0; i < jmlMahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
+            double rataRataMatkul = totalPerMatkul / jmlMahasiswa;
+            System.out.printf("Mata kuliah %d: %.2f%n", (j + 1), rataRataMatkul);
 
-            System.out.println("Mata kuliah " + (j+1) + ": " + totalPerMatkul/4);
         }
+
+
         sc.close();
     }
     
 }
+
+ // int[][] nilai = new int[4][3];
+
+        // for (int i = 0; i < nilai.length; i++) {
+        //     System.out.println("Input nilai mahsiswa ke-" + (i+1));
+        //     double totalPerSiswa = 0;
+
+        //     for (int j = 0; j < nilai[i].length; j++) {
+        //         System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+        //         nilai[i][j] = sc.nextInt();
+        //         totalPerSiswa += nilai[i][j];
+        //     }
+
+        //     System.out.println("nilai rata-rata: " + totalPerSiswa/3);
+        // }
+
+        // System.out.println("\n===================================");
+        // System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
+
+        // for (int j = 0; j < 3; j++) {
+        //     double totalPerMatkul = 0;
+            
+        //     for (int i = 0; i < 4; i++) {
+        //         totalPerMatkul += nilai[i][j];
+        //     }
+
+        //     System.out.println("Mata kuliah " + (j+1) + ": " + totalPerMatkul/4);
+        // }
