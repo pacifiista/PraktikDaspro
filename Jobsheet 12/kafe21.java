5cb5fc7907560c8417bf5a57f2a889221b54cc32
pacifiista <naurasfh2020@gmail.com>
2025-11-25 14:11:55 +0700

    percobaan 1& 2

--- Jobsheet 12/kafe21.java
+++ Jobsheet 12/kafe21.java
@@ -1,6 +1,12 @@
 public class kafe21 {
 
-    public static void menu() {
+    public static void main(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang di Kafe 21, " + namaPelanggan + "!");
+        
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian.");
+        } 
+
         System.out.println("========== Kafe 21 Menu ==========");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Capppuccino - Rp 20,000");
@@ -10,9 +16,26 @@ public class kafe21 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===================================");
         System.out.println("Silakan pilih menu:");
-        
+
+
     }
     public static void main(String[] args) {
-        menu();
+        main("Andi", true);
     }
+
+    // public static void menu() {
+    //     System.out.println("========== Kafe 21 Menu ==========");
+    //     System.out.println("1. Kopi Hitam - Rp 15,000");
+    //     System.out.println("2. Capppuccino - Rp 20,000");
+    //     System.out.println("3. Latte - Rp 22,000");
+    //     System.out.println("4. Teh Tarik - Rp 12,000");
+    //     System.out.println("5. Roti Bakar - Rp 10,000");
+    //     System.out.println("6. Mie Goreng - Rp 18,000");
+    //     System.out.println("===================================");
+    //     System.out.println("Silakan pilih menu:");
+        
+    // }
+    // public static void main(String[] args) {
+    //     menu();
+    // }
 }
