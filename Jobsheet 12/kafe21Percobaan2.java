d9b28fd834f208bd4472e0965cdea01e72dc87ca
pacifiista <naurasfh2020@gmail.com>
2025-11-25 14:42:04 +0700

    percobaan 2

---
+++ Jobsheet 12/kafe21Percobaan2.java
@@ -0,0 +1,24 @@
+public class kafe21Percobaan2 {
+    public static void main(String namaPelanggan, boolean isMember) { //fungsi paramenter percobaan 2
+        System.out.println("Selamat datang di Kafe 21, " + namaPelanggan + "!");
+        
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian.");
+        } 
+
+        System.out.println("========== Kafe 21 Menu ==========");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Capppuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===================================");
+        System.out.println("Silakan pilih menu:");
+
+
+    }
+    public static void main(String[] args) {
+        main("Andi", true); //memanggil fungsi main dengan parameter nama dan status member
+    }
+}
